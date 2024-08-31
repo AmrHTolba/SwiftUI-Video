6989e6bfd96906cff87802ac83020d2a5cfabe88
AmrFiqi <amr.h.elfiqi@gmail.com>
2024-08-31 20:39:06 +0300

    Initial Commit

---
+++ SwiftUI-Videos/SwiftUI_VideosApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_VideosApp.swift
+//  SwiftUI-Videos
+//
+//  Created by Amr El-Fiqi on 31/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_VideosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
